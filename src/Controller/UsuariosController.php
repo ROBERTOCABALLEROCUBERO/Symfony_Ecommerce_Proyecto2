3d342f08ca3f30137369d1a7c3660440250c8096
MarioTurrientes <91555848+MarioTurrientes@users.noreply.github.com>
2023-01-17 21:18:55 +0100

    Update UsuariosController.php

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -23,17 +23,19 @@ class UsuariosController extends AbstractController
     /**
      * @Route("/usuarios/registro", name="app_usuarios_registro")
      */
-    public function registro(UsuariosRepository $productosRepository, Request $request): Response
+    public function registro(UsuariosRepository $userRepository, Request $request): Response
     {
         $nombre = $request->query->get('nombre');
-        $apellido = $request->query->get('apellido');
-        $nombre = $request->query->get('nombre');
-        $nombre = $request->query->get('nombre');
-        $nombre = $request->query->get('nombre');
-        $nombre = $request->query->get('nombre');
-        $nombre = $request->query->get('nombre');
-        $nombre = $request->query->get('nombre');
-        $nombre = $request->query->get('nombre');
+        $apellido = $request->query->get('Apellido');
+        $email = $request->query->get('email');
+        $contrasena = $request->query->get('contrasena');
+        $fecha = $request->query->get('fecha');
+        $tarjeta = $request->query->get('tarjeta');
+        $titular = $request->query->get('titular');
+        $seguridad = $request->query->get('Seguridad');
+        $facturacion = $request->query->get('facturacion');
+
+        $userRepository->registro($nombre , $apellido, $email, $contrasena, $fecha, $tarjeta, $titular, $seguridad, $facturacion);
         return $this->render('usuarios/registro/index.html.twig', [
             'controller_name' => 'UsuariosController',
         ]);
