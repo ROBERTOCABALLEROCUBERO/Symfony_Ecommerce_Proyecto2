0d30104568159e4d254001764cd26e2a997ef50f
MarioTurrientes <91555848+MarioTurrientes@users.noreply.github.com>
2023-01-17 19:24:18 +0100

    UsuariosControlador

---
+++ src/Controller/UsuariosController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsuariosController extends AbstractController
+{
+    /**
+     * @Route("/usuarios", name="app_usuarios")
+     */
+    public function index(): Response
+    {
+        return $this->render('usuarios/index.html.twig', [
+            'controller_name' => 'UsuariosController',
+        ]);
+    }
+}
