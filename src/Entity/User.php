005c112b6b2190f15c36e47db9dff9fa7d67c6b8
ROBERTOCABALLEROCUBERO <91220443+ROBERTOCABALLEROCUBERO@users.noreply.github.com>
2023-01-20 19:00:21 +0100

    cambios_user

---
+++ src/Entity/User.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apellidos;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fechanac;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numtarjeta;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titular;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $codSeg;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $direccion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->nombre;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->nombre;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFechanac(): ?\DateTimeInterface
+    {
+        return $this->fechanac;
+    }
+
+    public function setFechanac(\DateTimeInterface $fechanac): self
+    {
+        $this->fechanac = $fechanac;
+
+        return $this;
+    }
+
+    public function getNumtarjeta(): ?string
+    {
+        return $this->numtarjeta;
+    }
+
+    public function setNumtarjeta(string $numtarjeta): self
+    {
+        $this->numtarjeta = $numtarjeta;
+
+        return $this;
+    }
+
+    public function getTitular(): ?string
+    {
+        return $this->titular;
+    }
+
+    public function setTitular(string $titular): self
+    {
+        $this->titular = $titular;
+
+        return $this;
+    }
+
+    public function getCodSeg(): ?string
+    {
+        return $this->codSeg;
+    }
+
+    public function setCodSeg(string $codSeg): self
+    {
+        $this->codSeg = $codSeg;
+
+        return $this;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): self
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+}
