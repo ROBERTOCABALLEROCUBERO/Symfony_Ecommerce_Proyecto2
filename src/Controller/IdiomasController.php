801af7bfcfbf2784e92857df7eddd78af99b81d5
12antonio <91555840+12antonio@users.noreply.github.com>
2023-02-17 17:59:48 +0100

    cambiar_idioma_1

---
+++ src/Controller/IdiomasController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IdiomasController extends AbstractController
+{
+    /**
+     * @Route("/idiomas", name="app_idiomas")
+     */
+    public function changeLanguage($locale, SessionInterface $session) {
+        $session -> set('_locale', $locale);
+
+        return new Response('Lenguaje cambiado a ' . $locale);
+    }
+}
+
+//Mario:
+//El controlador recibe un parametro $locale que corresponde al idioma seleccionado por el usuario
+//Se utiliza la session de symfony para almacenar el idioma seleccionado, devuelve una respuesta indicando el idioma seleccionado
